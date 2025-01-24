851eddd8f276d3ab11746a10ffd0fcc223efc15d
Abhi@thcoder <97733502+Abhithcoder@users.noreply.github.com>
2025-01-24 23:36:17 +0530

    First commit

--- src/app.js
+++ src/app.js
@@ -9,10 +9,23 @@ app.use("/test", (req,res)=>{
     res.send("Testing from server!")
 })
 
-app.use("/user", (req,res)=>{
-    res.send("User request fulfilled!")
+app.get("/user", (req,res)=>{
+    res.send({"first_name": "Abhishek Panwar", "Age": 21})
 })
 
-app.use("/", (req,res)=>{
-    res.send("Hello from server!")
-})  
+app.post("/user", (req,res)=>{
+    res.send("Saved user information succesfully!")
+})
+
+app.delete("/user", (req, res)=>{
+    res.send("Deleted user info successfully!")
+}) 
+// app.use("/", (req,res)=>{
+//     res.send("Hello from server!")
+// })
+
+//This will redirect all the requests method i.e. GET, POST, PUT, DELETE etc. to "/user"
+// app.use("/user", (req,res)=>{
+//     res.send("User request fulfilled!")
+// })
+
